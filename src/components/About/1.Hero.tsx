4b187ad7d3f215e64deedda4a891a9b1feb21428
ramsss-diego <ramosss.diego27@gmail..com>
2026-07-16 17:02:13 -0600

    se ajustan detalles en hero de about

--- src/components/About/1.Hero.tsx
+++ src/components/About/1.Hero.tsx
@@ -3,26 +3,26 @@ import hero from "@/assets/images/About/hero.png";
 export default function Hero() {
   return (
     <section className="bg-header-gradient text-white">
-      <div className="flex flex-col items-center lg:h-[430px] lg:flex-row">
+      <div className="flex flex-col items-center lg:h-[60vh] lg:min-h-[380px] lg:max-h-[600px] lg:flex-row">
 
         {/* Imagen */}
-        <div className="flex w-full justify-start lg:w-[35%] lg:h-full lg:items-end">
+        <div className="flex w-full justify-center lg:w-auto lg:justify-start lg:shrink-0 lg:h-full lg:items-end">
           <img
             src={hero}
             alt="Parque España"
-            className="w-[85%] max-w-[220px] object-contain lg:w-[85%] lg:max-w-none"
+            className="w-[90%] max-w-[220px] object-contain lg:w-auto lg:h-[90%] lg:max-w-none"
           />
         </div>
 
         {/* Contenido */}
-        <div className="flex w-full items-center justify-center px-6 py-8 lg:w-[65%] lg:h-full lg:px-0 lg:py-0">
-          <div className="w-full rounded-l-[70px] bg-gradient-to-r from-[#031826] to-[#073D5F] px-8 py-10 shadow-xl  lg:py-28">
+        <div className="flex w-full items-center justify-center px-6 py-8 lg:flex-1 lg:h-full lg:px-0 lg:py-0">
+          <div className="w-full rounded-3xl bg-gradient-to-r from-[#031826] to-[#073D5F] px-8 py-10 shadow-xl lg:rounded-none lg:rounded-l-[70px] lg:py-28">
 
-            <h1 className="text-lg font-bold leading-tight lg:text-4xl">
+            <h1 className="text-left! ml-5 text-lg font-bold leading-tight lg:text-4xl">
               Más de seis décadas construyendo comunidad
             </h1>
 
-            <p className="mt-6 max-w-3xl text-lg text-white/90 lg:text-2xl">
+            <p className="text-left! ml-5 mt-6 max-w-3xl text-lg text-white/90 lg:text-2xl">
               Tradición, bienestar y deporte para todas las generaciones.
             </p>
           </div>
