10cd2423b6e080125fc40380c51f1388ccb687f2
ramsss-diego <ramosss.diego27@gmail..com>
2026-07-16 17:57:25 -0600

    Se agrega StatsCounter a vista about

---
+++ src/constants/About.ts
@@ -0,0 +1,27 @@
+
+
+export const AboutContent = {
+  // hero: {
+  //   title: "Aquí se vive el movimiento",
+  //   description: "Deporte, cultura y comunidad en un mismo espacio.",
+  //   buttonText: "Descubre Parque España I",
+  //   buttonLink: "/parque-espana-1",
+  //   imageAlt:
+  //     "Jugador de tenis y mujer con vestimenta tradicional en Parque España",
+  // },
+
+  // stats: {
+  //   title: "Tradición, deporte y comunidad en Puebla",
+  //   description:
+  //     "Un espacio donde familias, cultura y bienestar se unen para crear experiencias memorables para todas las generaciones.",
+  //   buttonText: "Conoce nuestras instalaciones",
+  //   buttonLink: "/instalaciones",
+  //   counters: [
+  //     { value: 6500, suffix: "", label: "Usuarios activos" },
+  //     { value: 60, suffix: "", label: "Años de historia" },
+  //     { value: 10, suffix: "", label: "Disciplinas deportivas" },
+  //     { value: 1, suffix: "M", label: "de experiencias compartidas" },
+  //   ],
+  // },
+
+};
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 
 import Hero from '../components/About/1.Hero'
+import StatsCounter from "../components/Home/3.StatsCounter";
 
 export default function About() {
     return (
         <main>
             <Hero />
+            <StatsCounter />
         </main>
     )
 }
