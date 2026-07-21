36d3aef9d2384577142b7b58cb7855a47d63ce6e
ramsss-diego <ramosss.diego27@gmail..com>
2026-07-21 10:09:00 -0600

    Se agrega espaciado entre componentes de vista about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 
 import Hero from '../components/About/1.Hero'
-// import StatsCounter from "../components/Home/3.StatsCounter";
+import StatsCounter from "../components/StatsCounter";
 import Essence from "../components/About/3.Essence";
 import Pillars from "../components/About/4.Pillars";
 import MissionVision from "../components/About/6.MissionVision";
@@ -8,9 +8,9 @@ import JoinOurStory from "../components/About/7.JoinOurStory";
 
 export default function About() {
     return (
-        <main>
+        <main className="space-y-16 md:space-y-24">
             <Hero />
-            {/* <StatsCounter /> */}
+            <StatsCounter />
             <Essence />
             <Pillars />
             <MissionVision />
